fb8746dd7f451375a9f96f52b374310535f926c6
sebbyling <43594362+sebbyling@users.noreply.github.com>
2019-07-31 15:35:30 +1200

    Started Post Processing code

---
+++ Prototype2/Assets/Materials/ShaderTests/PostProcessingCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostProcessingCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Material ppMAT;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, ppMAT);
+    }
+}
