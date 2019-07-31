b9dd8dbbe05d5b4c564cd4fb60b5dadee8ec6477
Tim <timbbby@gmail.com>
2019-07-31 13:17:17 +1200

    Player Script Added

---
+++ Prototype2/Assets/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float distanceFromCenter = 1.0f;
+
+    private Transform initialPosition;
+
+    // The most recent directional key pressed by the player determines their position
+    Vector3 movePosition;
+    
+
+    private void Update()
+    {
+        // Change movePosition
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            movePosition = Vector3.forward;
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            movePosition = Vector3.back;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            movePosition = Vector3.left;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            movePosition = Vector3.right;
+        }
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
