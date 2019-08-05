5e4ffd438e326100f7b9dd62183b7908c1d3359f
StefTheDev <steffthedev@gmail.com>
2019-08-06 11:53:09 +1200

    Update Recipes
    Made Recipe's functional.

--- Prototype2/Assets/scripts/Foods/Food.cs
+++ Prototype2/Assets/scripts/Foods/Food.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+public enum FoodType
+{
+    APPLE,
+    ORANGE,
+    CHIPS
+}
 
 public class Food : MonoBehaviour
 {
     public bool isHealthy = true;
     public int movePosition;
     public FoodType type;
+    public Sprite sprite;
 
     public Food()
     {
