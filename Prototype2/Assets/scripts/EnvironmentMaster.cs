fd368247cfb4ebc06fa68f2fd971983d3d845de7
sebbyling <43594362+sebbyling@users.noreply.github.com>
2019-08-07 10:13:17 +1200

    Environment Script + ground Mesh

---
+++ Prototype2/Assets/scripts/EnvironmentMaster.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentMaster : MonoBehaviour
+{
+    //Scaling Environment
+    [Range(0.0f, 1.0f)]
+    public float environScale = 0.0f;
+
+    public GameObject prop1;
+
+    //Lighting
+    public Light thisLight;
+
+    [Range(0.0f, 10.0f)]
+    public float intenseVal = 0.0f;
+
+    [Range(0.0f, 1.0f)]
+    public float lightR = 0.0f;
+
+    [Range(0.0f, 1.0f)]
+    public float lightG = 0.0f;
+
+    [Range(0.0f, 1.0f)]
+    public float lightB = 0.0f;
+
+    Color lightCol;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisLight = thisLight.GetComponent<Light>();
+        lightR = 1.0f;
+        lightG = 1.0f;
+        lightB = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        prop1.transform.localScale = new Vector3(environScale, environScale, environScale);
+
+        thisLight.intensity = intenseVal;
+
+        lightCol = new Vector4(lightR, lightG, lightB, thisLight.color.a);
+
+        thisLight.color = lightCol;
+    }
+}
