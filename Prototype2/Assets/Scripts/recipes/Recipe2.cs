d4e7476d1504cab3a352103cb4c8bee0b4b7b602
StefTheDev <steffthedev@gmail.com>
2019-08-07 13:43:23 +1200

    New Recipe System
    Added new Recipe Manager
    Added new Recipe
    Added RecipeFood

---
+++ Prototype2/Assets/Scripts/recipes/Recipe2.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Recipe2 : MonoBehaviour
+{
+    public Image spacer;
+
+    private Food[] foods;
+    private List<RecipeFood> recipeFoods;
+
+    public void Initialise(int size)
+    {
+        if(foods == null) foods = Resources.LoadAll<Food>("Foods");
+
+        for (int i = 0; i < size; i++)
+        {
+            Sprite sprite = Random().sprite;
+            GameObject gameObject = new GameObject("Food");
+
+            RecipeFood recipeFood = gameObject.AddComponent<RecipeFood>();
+            recipeFood.SetConsumed(false);
+            recipeFood.GetImage().sprite = sprite;
+
+            recipeFoods.Add(recipeFood);
+        }
+    }
+
+    public Food Random()
+    {
+        return foods[UnityEngine.Random.Range(0, foods.Length)];
+    }
+
+    public List<RecipeFood> GetFood()
+    {
+        return recipeFoods;
+    }
+}
---
+++ Prototype2/Assets/Scripts/recipes/RecipeManager2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecipeManager2 : MonoBehaviour
+{
+    public GridLayoutGroup grid;
+    private Recipe2[] recipes2 = new Recipe2[2];
+
+    public void Create(int index)
+    {
+        GameObject gameObject = new GameObject("Recipe");
+        gameObject.transform.SetParent(transform);
+
+        Recipe2 recipe = gameObject.AddComponent<Recipe2>();
+        recipe.Initialise(Random.Range(2, 5));
+        recipes2[index] = recipe;
+    }
+
+    public void Refresh(Recipe2 recipe)
+    {
+        List<RecipeFood> recipeFoods = recipe.GetFood();
+        for(int i = 0; i < recipeFoods.Count; i++)
+        {
+
+        }
+    }
+}
